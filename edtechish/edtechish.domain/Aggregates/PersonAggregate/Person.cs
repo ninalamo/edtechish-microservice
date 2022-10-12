62fbf9074c8e20428cd185c18f9a55db80ad190a
Ninism <nin.alamo@outlook.com>
2022-10-12 20:15:20 +0800

    Create Person.cs

---
+++ edtechish/edtechish.domain/Aggregates/PersonAggregate/Person.cs
@@ -0,0 +1,43 @@
+using edtechish.domain.SeedWork;
+
+namespace edtechish.domain.Aggregates.PersonAggregate;
+
+public class Person : Entity<Guid>, IAggregateRoot
+{
+    public string? FirstName { get; private set; }
+    public string? LastName { get; private set; }
+    public string? MiddleName { get; private set; }
+    
+    public DateTime DateOfBirth { get; private set; }
+
+    #region Ctor
+    protected Person()
+    {
+        
+    }
+
+    public Person(string firstName, string lastName, string middleName, DateTime dateOfBirth)
+    {
+        SetName(firstName, lastName, middleName);
+        SetDateOfBirth(dateOfBirth);
+    }
+    #endregion
+
+    #region Behaviors
+    public void SetName(string firstName, string lastName, string middleName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        MiddleName = middleName;
+    }
+
+    public void SetDateOfBirth(DateTime dateOfBirth) => DateOfBirth = dateOfBirth;
+    
+    
+    
+    public string GetFullName() => $"{FirstName} {MiddleName} {LastName}";
+    
+    #endregion
+
+}
+
